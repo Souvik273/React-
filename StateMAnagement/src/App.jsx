8360d363089f50123951b815f73aa14478758da1
Souvik273 <souvikgoswami464@gmail.com>
2024-12-25 13:24:21 +0530

    Basic Application committed

---
+++ StateMAnagement/src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import CounterAppl from './components/CounterAppl'
+import FetchData from './components/FetchData'
+
+function App() {
+  
+  return (
+    <>
+      <CounterAppl/>
+      <FetchData/>
+    </>
+  )
+}
+
+export default App
